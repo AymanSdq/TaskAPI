57a6cb69398672c0ad8b0e518a02a1389997e10d
AymanSdq <sedqiayman02@gmail.com>
2025-03-10 23:44:08 +0000

    Only a route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import { Request , Response } from "express-serve-static-core";
 
 const app = express();
 
-
+app.get("/", (req : Request, res : Response) => {
+    res.status(202).json({Message : "Hello World"})
+})
 
 app.listen(3000, () => {
     console.log("Server is On! ")
