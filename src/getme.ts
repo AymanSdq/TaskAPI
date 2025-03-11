d17e3f0ebcad467b22554ac75a0b8c2fccdeec0b
AymanSdq <sedqiayman02@gmail.com>
2025-03-11 23:11:39 +0000

    Pushing Nothing

---
+++ src/getme.ts
@@ -0,0 +1,12 @@
+
+
+export default async function  callmePlease( request : Request , response : Response) {
+
+    console.log(request.body)
+    
+    try {  
+
+    }catch(err){
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import express from "express";
-import { Request , Response } from "express-serve-static-core";
+import  express, { request } from "express";
+import { Request, Response  } from "express";
 
 const app = express();
 
-app.get("/", (req : Request, res : Response) => {
-    res.status(202).json({Message : "Hello World"})
+app.use(express.json())
+
+
+app.get("/dossier", (req : Request , res : Response ) => {
+    res.status(202).send("Hello Ayman")
 })
 
-app.listen(3000, () => {
-    console.log("Server is On! ")
+app.post("/showme", async (req : Request , res : Response ) => {
+
+
+    const { email , password} : {email : string, password : string} = await req.body
+
+    try {
+
+        
+    } catch (error) {
+
+    }
+
+
 })
+
+
+app.listen(3000, () => {
+    console.log("Server is Up On : http://localhost:3000")
+})
+
+
