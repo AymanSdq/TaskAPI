cd837171b3d15b1c892d629fb8a862b5344d7c0c
AymanSdq <sedqiayman02@gmail.com>
2025-03-13 17:48:27 +0000

    User controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,26 @@
 import { Request , Response } from "express-serve-static-core";
 
-export const registerUser = async ( request : Request, response : Response ) => {
 
+interface userRegister {
+    fullName : string,
+    email : string,
+    password : string
+}
+
+
+// Register Controller 
+
+export const registerUser = async ( request : Request, response : Response ) => {
     try {
 
-        response.status(202).send(" Welcome to Register Page !")
+        const { fullName , email, password } : userRegister = request.body
+
+        response.status(202).json({ fullName : fullName, })
 
     } catch (error) {
         response.status(502).json({ ErrorMessage : error })
     }
 
 }
+
+// Login Controller
