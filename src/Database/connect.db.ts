3e2b85314dd54cad203ea0943f8f4949c6bae5d1
AymanSdq <sedqiayman02@gmail.com>
2025-03-16 18:09:56 +0000

    Changing db from Local to NEON !

--- src/Database/connect.db.ts
+++ src/Database/connect.db.ts
@@ -1,24 +1,28 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const Port = 5432
 
-// Database Data
+const connectionString = process.env.DATABASE_URL
+
+
 const databaseConnection = new Pool({
-    user : process.env.db_username,
-    host : process.env.db_host,
-    database : process.env.db_name,
-    password : process.env.db_password,
-    port  : Port
-})
-databaseConnection.connect();
+    connectionString: connectionString
+});
 
-// Checking for db Errors 
+
+databaseConnection.connect()
+    .then(() => console.log('Connected to PostgreSQL database successfully!'))
+    .catch((err) => {
+        console.error('Error connecting to the database:', err.message);
+        process.exit(-1);
+    });
+
 databaseConnection.on('error', (err) => {
-    console.log({ErrorDatabase : err.message });
-    process.exit(-1)
-})
+    console.error('Database error:', err.message);
+    process.exit(-1);
+});
 
-export const query = (text: string, params: any[]) => databaseConnection.query(text, params)
+
+export const query = (text: string, params: any[]) => databaseConnection.query(text, params);
